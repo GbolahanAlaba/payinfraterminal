d279a26055e4937a4ca3cb1b9e5006f667a1a4f6
Gbolahan Alaba <thegbolahanalaba@gmail.com>
2026-03-03 14:17:49 +0100

    Paystack and Flutterwave integration done

--- payinfra/settings/base.py
+++ payinfra/settings/base.py
@@ -188,7 +188,7 @@ EMAIL_TIMEOUT = 300
 SUPPORT_EMAIL = "support@payinfra.co"
 EMAIL_PREFIX = "iGospel"
 
-FERNET_SECRET_KEY = os.environ.get("FERNET_SECRET_KEY").encode()
+# FERNET_SECRET_KEY = os.environ.get("FERNET_SECRET_KEY").encode()
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "Pay Infra Terminal API",
