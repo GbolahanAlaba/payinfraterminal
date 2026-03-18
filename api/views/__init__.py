29364e0e1aea369d1ab05cacff98b38b5de9294f
Gbolahan Alaba <thegbolahanalaba@gmail.com>
2026-03-18 03:33:08 +0100

    Celery and Webhook

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -3,7 +3,7 @@ API Views Package
 All payment views for the APIs service
 """
 
-from .payment import ProcessPaymentAPIView
+from .initiate_payment import ProcessPaymentAPIView
 from .verify_transaction import VerifyTransactionView
 from .client_provider_api import SetupClientProviderAPIView
 from .client import RegenerateAPIKeysView, UpdateWebhookURLView
